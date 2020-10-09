fcf4498737ac1ecfe3b67cb994bffc330b3744da
hellobrian <brianhan@hey.com>
2020-10-08 23:07:23 -0500

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '80989b70f13865959f7113b8aa5e0fbb'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd75bae47-a751-439a-9e3e-a87b22bcfca0'
+};
